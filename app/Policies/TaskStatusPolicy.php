d794df7216520206d410b520c1fcaf975134a7e3
Kromian1 <mok1408@yandex.ru>
2026-05-22 16:34:00 +0300

    добавлена политика авторизации для TaskStatus

--- app/Policies/TaskStatusPolicy.php
+++ app/Policies/TaskStatusPolicy.php
@@ -9,25 +9,16 @@ use Illuminate\Auth\Access\Response;
 
 class TaskStatusPolicy
 {
-    /**
-     * Determine whether the user can view any models.
-     */
     public function viewAny(User $user): bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
     public function view(User $user, TaskStatus $taskStatus): bool
     {
         return true;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
     public function create(User $user): Response
     {
         return Auth::check()
@@ -35,9 +26,6 @@ class TaskStatusPolicy
             : Response::deny('У вас недостаточно прав для создания статуса');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, TaskStatus $taskStatus): Response
     {
         return Auth::check()
@@ -45,9 +33,6 @@ class TaskStatusPolicy
             : Response::deny('У вас недостаточно прав для изменения статуса');
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
     public function delete(User $user, TaskStatus $taskStatus): Response
     {
         return Auth::check()

d86e653be5453de00ef2118fbdb19b13fe3740a0
m.kuznetsov <mok1408@yandex.ru>
2026-05-27 14:29:35 +0300

    фикс отображения кнопок авторизации и регистрации + логаута

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -12,9 +12,6 @@
 
                 <!-- Navigation Links -->
                 <div class="space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('task_statuses.index')" :active="request()->routeIs('task_statuses.index')">
                         {{ __('Statuses') }}
                     </x-nav-link>
@@ -26,7 +23,7 @@
 
             <!-- Settings Dropdown -->
             @auth
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="sm:flex sm:items-center sm:ms-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
@@ -56,10 +53,10 @@
                     </x-dropdown>
                 </div>
             @else
-                <div class="hidden sm:flex sm:items-center sm:ms-6">
+                <div class="sm:flex sm:items-center sm:ms-6">
                     <div class="space-x-4">
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-                        <a href="{{ route('register') }}" class="text-sm text-gray-700 underline">Register</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">{{ __('Login') }}</a>
+                        <a href="{{ route('register') }}" class="text-sm text-gray-700 underline">{{ __('Register') }}</a>
                     </div>
                 </div>
             @endauth
@@ -85,6 +82,9 @@
                 </x-responsive-nav-link>
                 <x-responsive-nav-link :href="route('task_statuses.index')" :active="request()->routeIs('task_statuses.*')">
                     {{ __('Statuses') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('tasks.index')" :active="request()->routeIs('tasks.*')">
+                    {{ __('Tasks') }}
                 </x-responsive-nav-link>
             </div>
 

4d72882b1e70c5f2b2b78b852aa3b93f6fd1c418
m.kuznetsov <mok1408@yandex.ru>
2026-05-28 09:59:18 +0300

    созданы связи между Label и Task

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Label extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name', 'description'];
+
+    public function tasks(): belongsToMany
+    {
+        return $this->belongsToMany(Task::class, 'labels');
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Task extends Model
 {
@@ -30,5 +32,10 @@ class Task extends Model
     public function assignee(): belongsTo
     {
         return $this->belongsTo(User::class, 'assigned_to_id');
+    }
+
+    public function labels(): belongsToMany
+    {
+        return $this->belongsToMany(Label::class, 'labels');
     }
 }

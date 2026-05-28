47ecc172283cd0fb3689255c974996b773682e61
m.kuznetsov <mok1408@yandex.ru>
2026-05-28 10:21:51 +0300

    добавил политику LabelPolicy

---
+++ app/Policies/LabelPolicy.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Label;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
+
+class LabelPolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Label $label): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return Auth::check()
+            ? Response::allow()
+            : Response::deny(__('You do not have permission to create label'));
+
+    }
+
+    public function update(User $user, Label $label): bool
+    {
+        return Auth::check()
+            ? Response::allow()
+            : Response::deny(__('You do not have permission to update label'));
+
+    }
+
+    public function delete(User $user, Label $label): bool
+    {
+        return Auth::check()
+            ? Response::allow()
+            : Response::deny(__('You do not have permission to delete label'));
+
+    }
+}

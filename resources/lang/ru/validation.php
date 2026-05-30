a94127a99a434078eaca9e096b074d8d8741245f
Kromian1 <mok1408@yandex.ru>
2026-05-30 15:44:29 +0300

    поправил переводы

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'required' => 'Поле :attribute обязательно для заполнения.',
+    'unique' => 'Такое значение поля :attribute уже существует.',
+    'exists' => 'Выбранное значение для :attribute некорректно.',
+    'min' => [
+        'string' => 'Поле :attribute должно содержать минимум :min символов.',
+    ],
+    'attributes' => [
+        'name' => 'Название',
+        'status_id' => 'Статус',
+        'assigned_to_id' => 'Исполнитель',
+        'description' => 'Описание',
+        'labels' => 'Метки',
+    ],
+];

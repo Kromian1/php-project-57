8f217db79b2b20ab388ff2ea847603a5b8ac94e5
Kromian1 <mok1408@yandex.ru>
2026-05-30 22:48:35 +0300

    Обновлен перевод

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,9 @@
 
 @section('title', __('Main page'))
 
-@section('header', __('Main page'))
-
 @section('content')
-    <div>
-        This is main page!
+    <div class="w-full px-6 py-12 bg-white text-black min-h-screen">
+        <h1 style="font-size: 2.5rem; font-weight: 700; line-height: 1.1;" class="tracking-tight mb-6 text-black">{{ __('Hello from Hexlet!') }}</h1>
+        <h3 class="text-xl font-normal text-gray-400 tracking-wide">{{ __('This is simple task manager on Laravel') }}</h3>
     </div>
 @endsection

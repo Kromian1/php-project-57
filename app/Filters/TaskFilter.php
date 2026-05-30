9cffe9c0303cb9a15c11f635410681702ff51a1b
Kromian1 <mok1408@yandex.ru>
2026-05-30 20:50:58 +0300

    улучшена верстка

--- app/Filters/TaskFilter.php
+++ app/Filters/TaskFilter.php
@@ -4,21 +4,21 @@ namespace App\Filters;
 
 class TaskFilter extends QueryFilter
 {
-    public function status_id(int $id = null)
+    public function statusId(int $id = null)
     {
         return $this->builder->when($id, function ($query) use ($id) {
             $query->where('status_id', $id);
         });
     }
 
-    public function created_by_id(int $id = null)
+    public function createdById(int $id = null)
     {
         return $this->builder->when($id, function ($query) use ($id) {
             $query->where('created_by_id', $id);
         });
     }
 
-    public function assigned_to_id(int $id = null)
+    public function assignedToId(int $id = null)
     {
         return $this->builder->when($id, function ($query) use ($id) {
             $query->where('assigned_to_id', $id);

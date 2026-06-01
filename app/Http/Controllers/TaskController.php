158a657cebf2b7339d9b10564276aa0cedd425a0
m.kuznetsov <mok1408@yandex.ru>
2026-06-01 12:31:51 +0300

    fix

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -43,7 +43,7 @@ class TaskController extends Controller
             'name' => 'required|min:1',
             'description' => 'nullable',
             'status_id' => 'required|exists:task_statuses,id',
-            'assigned_to_id' => 'nullable|exists:users,id',
+            'assigned_to_id' => 'required|exists:users,id',
             'labels' => 'nullable|array',
             'labels.*' => 'exists:labels,id',
         ]);

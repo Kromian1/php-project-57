366a4c9d1eabb77f27ed1828c10d9e2be9d1c04d
m.kuznetsov <mok1408@yandex.ru>
2026-06-09 14:42:37 +0300

    убрано дублирование ошибок валидации из форм

--- resources/views/tasks/form.blade.php
+++ resources/views/tasks/form.blade.php
@@ -1,13 +1,3 @@
-@if ($errors->any())
-    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-6">
-        <ul class="list-disc pl-5">
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
 <div class="space-y-6">
     <div>
         {{ html()->label(__('task.name'), 'name')->class('block text-gray-700 font-bold mb-2') }}
